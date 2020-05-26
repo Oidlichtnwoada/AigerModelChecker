4df1c8e176c48637384af637afb18fb949af2d8b
Hannes Brantner <e01614466@student.tuwien.ac.at>
2020-05-26 08:11:12 +0200

    added gitignore for build files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-sudo apt update && sudo apt install pypy3
+sudo apt update && sudo apt install python3.8 python3.8-dev cython
 cd ../picosat && ./configure && make clean && ./configure && make
 cd ../aiger && ./configure && make clean && ./configure && make
 cd ../minisat/core && make clean && make
 cd ../simp && make clean && make
+cd ../../src
+cython ./bmc.py -3 --embed
+
