9a605a565161b22df18b3e4599a5d207d6bedf2e
Hannes Brantner <e01614466@student.tuwien.ac.at>
2020-05-28 17:28:54 +0200

    changes to include minisat_proof

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,3 +4,4 @@ cd ../picosat && ./configure && make clean && ./configure && make
 cd ../aiger && ./configure && make clean && ./configure && make
 cd ../minisat/core && make clean && make
 cd ../simp && make clean && make
+cd ../../minisat_proof && make clean && make
