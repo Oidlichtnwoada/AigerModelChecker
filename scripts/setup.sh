7075992082f2cf1f83450c9db681dbb494f10d36
Hannes Brantner <hannes.brantner@hotmail.com>
2020-09-12 13:03:28 +0200

    added missing library to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git submodule update --init --recursive
-sudo apt update && sudo apt install pypy3 -y
+sudo apt update && sudo apt install pypy3 zlib1g-dev -y
 cd ../picosat && ./configure && make clean && ./configure && make
 cd ../aiger && ./configure && make clean && ./configure && make
 cd ../minisat/core && make clean && make
