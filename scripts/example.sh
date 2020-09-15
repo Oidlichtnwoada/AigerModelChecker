40d44ccb16952b3270159c496ec6f8ee2d969529
Hannes Brantner <hannes.brantner@hotmail.com>
2020-09-16 00:43:54 +0200

    improved test cases

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
+
+./run-part1.sh ../models/texas.ifetch1^5.E.aag 0
+./run-part1.sh ../models/texas.ifetch1^5.E.aag 1
+./run-part1.sh ../models/texas.ifetch1^5.E.aag 2
 ./run-part1.sh ../models/texas.ifetch1^5.E.aag 19
 ./run-part1.sh ../models/texas.ifetch1^5.E.aag 20
+./run-part1.sh ../models/texas.ifetch1^5.E.aag 50
+./run-part1.sh ../models/texas.ifetch1^5.E.aag 100
+
+./run-part1.sh ../models/vis.eisenberg.E.aag 0
+./run-part1.sh ../models/vis.eisenberg.E.aag 1
+./run-part1.sh ../models/vis.eisenberg.E.aag 2
 ./run-part1.sh ../models/vis.eisenberg.E.aag 19
 ./run-part1.sh ../models/vis.eisenberg.E.aag 20
+./run-part1.sh ../models/vis.eisenberg.E.aag 50
+./run-part1.sh ../models/vis.eisenberg.E.aag 100
+
+./run-part1.sh ../models/texas.two_proc^1.E.aag 0
+./run-part1.sh ../models/texas.two_proc^1.E.aag 1
+./run-part1.sh ../models/texas.two_proc^1.E.aag 2
 ./run-part1.sh ../models/texas.two_proc^1.E.aag 13
 ./run-part1.sh ../models/texas.two_proc^1.E.aag 14
+./run-part1.sh ../models/texas.two_proc^1.E.aag 50
+./run-part1.sh ../models/texas.two_proc^1.E.aag 100
+
 ./run-part2.sh ../models/nusmv.syncarb5^2.B.aag
+
 ./run-part2.sh ../models/vis.emodel.E.aag
+
 ./run-part2.sh ../models/cmu.gigamax.B.aag
--- src/test_bmc.py
+++ src/test_bmc.py
@@ -5,7 +5,7 @@ from subprocess import run, PIPE
 class BmcTestCase(unittest.TestCase):
     def test_bmc(self):
         script_output = run('./example.sh', cwd='../scripts', shell=True, stdout=PIPE).stdout.decode('utf-8')
-        expected_output = 'OK\nFAIL\nOK\nFAIL\nOK\nFAIL\nOK\nOK\nOK\n'
+        expected_output = 'OK\nOK\nOK\nOK\nFAIL\nFAIL\nFAIL\n' * 3 + 'OK\n' * 3
         self.assertEqual(script_output, expected_output)
 
 
