da01497597967e902f66b4d3531d84e8b6b3c4c0
Hannes Brantner <hannes.brantner@hotmail.com>
2020-09-17 00:56:34 +0200

    extended test cases

--- src/test_bmc.py
+++ src/test_bmc.py
@@ -4,11 +4,17 @@ from subprocess import run, PIPE
 
 PART1_MODELS = [('texas.ifetch1^5.E', 19),
                 ('vis.eisenberg.E', 19),
-                ('texas.two_proc^1.E', 13)]
+                ('texas.two_proc^1.E', 13),
+                ('texas.two_proc^2.E', 14),
+                ('texas.two_proc^5.E', 13),
+                ('cmu.gigamax.B', inf)]
 
 PART2_MODELS = [('nusmv.syncarb5^2.B', inf),
                 ('vis.emodel.E', inf),
-                ('cmu.gigamax.B', inf)]
+                ('cmu.gigamax.B', inf),
+                ('ken.flash^13.C', inf),
+                ('nusmv.syncarb10^2.B', inf),
+                ('texas.ifetch1^8.E', 3)]
 
 
 def get_output(boolean):
@@ -21,6 +27,7 @@ def get_output(boolean):
 class BmcTestCase(unittest.TestCase):
     def test_part1(self):
         for model_name, safe_bound in PART1_MODELS:
+            print(f'testing part1 for {model_name} ...')
             bounds = set([2 ** i for i in range(8)])
             if safe_bound != inf:
                 bounds = bounds.union({safe_bound, safe_bound + 1})
@@ -31,6 +38,7 @@ class BmcTestCase(unittest.TestCase):
 
     def test_part2(self):
         for model_name, safe_bound in PART2_MODELS:
+            print(f'testing part2 for {model_name} ...')
             script_output = run(f'./run-part2.sh ../models/{model_name}.aag', cwd='../scripts', shell=True, stdout=PIPE).stdout.decode('utf-8').strip()
             expected_output = get_output(safe_bound == inf)
             self.assertEqual(script_output, expected_output)
